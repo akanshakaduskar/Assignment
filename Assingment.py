18c073aad811388fb88660bcd786342ea00ecf8a
akanshakaduskar <akanshakaduskar@gmail.com>
2024-06-06 13:07:28 +0530

    Assignment_1

---
+++ Assingment.py
@@ -0,0 +1,48 @@
+''' 1)	accept a number and display its table.   '''   
+# num = int(input("Enter a number : "))
+# print("Multiplication table of ", num)
+# for i in range(1,11):
+#     print(num, "x", i, "=", num*i)
+
+'''2)	using switch ….case   display whether accepted character is vowel or not.'''
+# value = input("Enter a character: ")
+
+# if value in 'aeiouAEIOU':
+#     print(f"The accepted character {value} is a vowel.")
+# else:
+#     print(f"The accepted character {value} is not a vowel.")
+
+
+'''3)	Display numbers  1 to 10 using  While loop'''
+# num = 1
+# print("Displaying numbers from 1 to 10")
+# while num <= 10:
+#     print(num)
+#     num += 1
+
+'''4)	Display numbers from 3 to 30 except number 24  using while loop.'''
+# num = 3
+
+# while num <= 30:
+#     if num != 24:
+#         print(num)
+#     num += 1
+
+'''5)	accept marks from the user. Using if…….elif….  Else,  display whether result is  fail, pass, second class , first class, Distinction etc.'''
+# marks = float(input("Enter the marks: "))
+
+# if marks < 0 or marks > 100:
+#     print("Invalid marks! Marks should be between 0 and 100.")
+# elif marks < 40:
+#     print("Result: Fail")
+# elif marks < 60:
+#     print("Result: Pass")
+# elif marks < 75:
+#     print("Result: Second Class")
+# elif marks < 90:
+#     print("Result: First Class")
+# else:
+#     print("Result: Distinction")
+
+
+
